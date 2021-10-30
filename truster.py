140ef2700828d3c3e87185bc76884af4c60e9d55
EdoardoRaimondi <edoardo.raimondi@studenti.unipd.it>
2021-10-30 19:16:39 +0200

    Added analyzer class with the (basics) methods for the goodnees score calculation (both node and graph level)

--- truster.py
+++ truster.py
@@ -6,6 +6,8 @@ from networkx.algorithms.centrality.closeness import closeness_centrality
 from networkx.classes.function import is_directed, number_of_nodes, subgraph
 import pandas as pd
 
+from Graph_analyzer import Graph_analyzer
+
 # This programs analyize Bitcoin trust network transaction. 
 #
 # The file columns format is:
@@ -16,67 +18,81 @@ import pandas as pd
 # RATING: the source's rating for the target, ranging from -10 to +10 in steps of 1
 # TIME: the time of the rating, measured as seconds since Epoch.
 
-# --------------------------------- 
-#        FILE MANIPULATION
-# ---------------------------------
-# read csv file
-df = pd.read_csv('soc-sign-bitcoinotc.csv')
-# clean it (remove last column)
-df.drop(df.columns[len(df.columns)-1], axis=1, inplace=True)
-columns_name = ['source', 'target', 'weight']
-# add columns names 
-df.columns = columns_name
-# transform pandas dataframe into a directed graph
-graph = nx.from_pandas_edgelist(df, 'source', 'target', ['weight'], create_using=nx.DiGraph())
+def main():  
 
-# ---------------------------------
-#             ANALYSES
-# ---------------------------------
 
-# print the node with the most closennes centrality score
-# what does closeness centrality mean in our graph? Has it sense to consider it?
-start_time = time.monotonic()
-centrality_nodes = closeness_centrality(graph)
-end_time = time.monotonic()
-max_centrality_node = max(centrality_nodes, key = lambda x: centrality_nodes[x])
-print("max closeness node, closeness: {}, {}".format(max_centrality_node, centrality_nodes[max_centrality_node])) 
-print("execution time: {}".format(end_time-start_time)) #my pc 90sec
+    # --------------------------------- 
+    #        FILE MANIPULATION
+    # ---------------------------------
+    # read csv file
+    df = pd.read_csv('soc-sign-bitcoinotc.csv')
+    # clean it (remove last column)
+    df.drop(df.columns[len(df.columns)-1], axis=1, inplace=True)
+    columns_name = ['source', 'target', 'weight']
+    # add columns names 
+    df.columns = columns_name
+    # transform pandas dataframe into a directed graph
+    graph = nx.from_pandas_edgelist(df, 'source', 'target', ['weight'], create_using=nx.DiGraph())
 
-# print the node with th emost between centrality score
-# is betweenness centrality interesting or not? What is it mean?
-start_time = time.monotonic()
-betweenness_nodes = betweenness_centrality(graph)
-end_time = time.monotonic()
-max_betweennes_node = max(betweenness_nodes, key = lambda x: betweenness_nodes[x])
-print("max betweenness node, betweenness: {}, {}".format(max_betweennes_node, betweenness_nodes[max_betweennes_node]))  
-print("execution time: {}".format(end_time-start_time)) #my pc 196sec
+    # ---------------------------------
+    #             ANALYSES
+    # ---------------------------------
 
-# calculate the degree for all nodes
-# print out the node with greater degree and its value
-start_time = time.monotonic()
-degree_nodes = dict(graph.degree(graph.nodes)) # need a conversion tu use always the same lambda funciton
-end_time = time.monotonic()
-max_degree_node = max(degree_nodes, key = lambda x: degree_nodes[x])
-print("node: {}, degree: {}".format(max_degree_node, degree_nodes[max_degree_node]))
-print("execution time: {}".format(end_time-start_time)) #my pc 0.01sec
+    analyzer = Graph_analyzer(graph)
 
-# firs biefly analyses
-print("the node with greater centrality {}, has degree {} and betweenness {}".format(max_centrality_node, degree_nodes[max_centrality_node] ,betweenness_nodes[max_centrality_node]))
-print("the node with greater betweenness {}, has degree {} and centrlity {}".format(max_betweennes_node, degree_nodes[max_betweennes_node] ,centrality_nodes[max_betweennes_node]))
-print("the node with greater degree {}, has centrality {} and betweenness {}".format(max_degree_node, centrality_nodes[max_degree_node] ,betweenness_nodes[max_degree_node]))
+    # graph goodnees score
+    print("goodnees of a random node : {}".format(analyzer.node_goodness(905)))
+    nodes_goodness = analyzer.graph_goodness(5)
+    for node in analyzer.graph_goodness(5).keys():
+        print(" Node {} has a goodnees score of : {}".format(node, nodes_goodness[node]))
 
-# ----------------------------------------------------------
-#                   PRINT SOME SUBGRAPHS
-# ----------------------------------------------------------
+    # print the node with the most closennes centrality score
+    # what does closeness centrality mean in our graph? Has it sense to consider it?
+    start_time = time.monotonic()
+    centrality_nodes = closeness_centrality(graph)
+    end_time = time.monotonic()
+    max_centrality_node = max(centrality_nodes, key = lambda x: centrality_nodes[x])
+    print("max closeness node, closeness: {}, {}".format(max_centrality_node, centrality_nodes[max_centrality_node])) 
+    print("execution time: {}".format(end_time-start_time)) #my pc 90sec
 
-# subgraph with the node that has max centrality
-subnodes = graph.neighbors(max_centrality_node)
-subgraph = graph.subgraph(subnodes)
-figure1 = plt.subplot(121)
-nx.draw(subgraph, with_labels=True)
+    # print the node with th emost between centrality score
+    # is betweenness centrality interesting or not? What is it mean?
+    start_time = time.monotonic()
+    betweenness_nodes = betweenness_centrality(graph)
+    end_time = time.monotonic()
+    max_betweennes_node = max(betweenness_nodes, key = lambda x: betweenness_nodes[x])
+    print("max betweenness node, betweenness: {}, {}".format(max_betweennes_node, betweenness_nodes[max_betweennes_node]))  
+    print("execution time: {}".format(end_time-start_time)) #my pc 196sec
 
-# subgraph with the node that has max degree
-subnodes = graph.neighbors(max_degree_node)
-subgraph = graph.subgraph(subnodes)
-figure1 = plt.subplot(121)
-nx.draw(subgraph, with_labels=True)
+    # calculate the degree for all nodes
+    # print out the node with greater degree and its value
+    start_time = time.monotonic()
+    degree_nodes = dict(graph.in_degree(graph.nodes)) # need a conversion tu use always the same lambda funciton
+    end_time = time.monotonic()
+    max_degree_node = max(degree_nodes, key = lambda x: degree_nodes[x])
+    print("node: {}, degree: {}".format(max_degree_node, degree_nodes[max_degree_node]))
+    print("execution time: {}".format(end_time-start_time)) #my pc 0.01sec
+
+    # first biefly analyses
+    print("the node with greater centrality {}, has degree {} and betweenness {}".format(max_centrality_node, degree_nodes[max_centrality_node] ,betweenness_nodes[max_centrality_node]))
+    print("the node with greater betweenness {}, has degree {} and centrlity {}".format(max_betweennes_node, degree_nodes[max_betweennes_node] ,centrality_nodes[max_betweennes_node]))
+    print("the node with greater degree {}, has centrality {} and betweenness {}".format(max_degree_node, centrality_nodes[max_degree_node] ,betweenness_nodes[max_degree_node]))
+
+    # ----------------------------------------------------------
+    #                   PRINT SOME SUBGRAPHS
+    # ----------------------------------------------------------
+
+    # subgraph with the node that has max centrality
+    subnodes = graph.neighbors(max_centrality_node)
+    subgraph = graph.subgraph(subnodes)
+    figure1 = plt.subplot(121)
+    nx.draw(subgraph, with_labels=True)
+
+    # subgraph with the node that has max degree
+    subnodes = graph.neighbors(max_degree_node)
+    subgraph = graph.subgraph(subnodes)
+    figure1 = plt.subplot(121)
+    nx.draw(subgraph, with_labels=True)
+
+if __name__ == "__main__":
+    main()
